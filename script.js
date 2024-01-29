0d72c16fc6228de8111420615dd3c5d65d64a73e
Mady-T <adithya5463sajith@gmail.com>
2024-01-30 00:36:46 +0530

    Add randomized and darkening color, Add resizing

--- script.js
+++ script.js
@@ -1,13 +1,56 @@
-let boardSize = 16;
 const canvas = document.querySelector('main');
 
-for (let i = 0; i < boardSize; i++) {
-    let pixelRow = document.createElement('div');
-    pixelRow.classList.add('pixel-line');
-    for (let j = 0; j < boardSize; j++) {
-        let pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        pixelRow.appendChild(pixel);
+drawCanvas(16);
+
+const resetButton = document.querySelector('button');
+resetButton.addEventListener('click', () => {
+    const inp = prompt("Enter size of canvas (< 100 squares)");
+    drawCanvas(inp);
+});
+
+function drawCanvas(boardSize = 16) {
+    if (boardSize > 100 || boardSize < 0) {
+        alert('Invalid input!');
     }
-    canvas.appendChild(pixelRow);
+    
+    let lastChild = canvas.lastElementChild;
+    while (lastChild) {
+        canvas.removeChild(lastChild);
+        lastChild = canvas.lastElementChild;
+    }
+
+    for (let i = 0; i < boardSize; i++) {
+        let pixelRow = document.createElement('div');
+        pixelRow.classList.add('pixel-line');
+        for (let j = 0; j < boardSize; j++) {
+            let pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            pixel.addEventListener('mouseenter', (e) => {
+                if (!e.shiftKey) {
+                    pixel.style.backgroundColor = incrementAlpha(pixel.style.backgroundColor);
+                }
+                console.log(incrementAlpha(pixel.style.backgroundColor));
+            });
+            pixelRow.appendChild(pixel);
+        }
+        canvas.appendChild(pixelRow);
+    }
+}
+
+function randomizeColor() {
+    const red = Math.floor(Math.random()*255);
+    const green = Math.floor(Math.random()*255);
+    const blue = Math.floor(Math.random()*255);
+    return `rgba(${red},${green},${blue})`;
+}
+
+function incrementAlpha(rgba) {
+    if (typeof(rgba) !== 'string') {
+        return 'rgba(0,0,0,0.1)';
+    }
+    const lowerBound = rgba.lastIndexOf(',');
+    const upperBound = rgba.lastIndexOf(')');
+    const alpha = +rgba.slice(lowerBound+1, upperBound);
+    const newAlpha = alpha + 0.1;
+    return randomizeColor(rgba).replace(")" , `, ${newAlpha})`);
 }
